bd51f161e929821e8581c95e554b85cbe0ba778a
Amaranese <alejandromaranes@gmail.com>
2022-06-21 12:31:49 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateColumns: {
+        'auto-fit': 'repeat(auto-fit, minmax(280px, 1fr))',
+        'auto-fill': 'repeat(auto-fill, minmax(290px, 1fr))',
+      },
+      spacing: {
+        chat: '88vh',
+        fullscreen: '80vh',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
